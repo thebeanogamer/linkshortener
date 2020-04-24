4c72ed9208c9035f8656fb5ad4ccca956d111fb5
Daniel Milnes <thebeanogamer@gmail.com>
2020-04-24 23:04:08 +0100

    Sort table output

--- linkshortener/notifications.py
+++ linkshortener/notifications.py
@@ -17,7 +17,7 @@ def generate():
             ),
         )
         .get_template("email_template.html")
-        .render(date=date.today(), links=db.scan()["Items"])
+        .render(date=date.today(), links=sorted(db.scan()["Items"], lambda x: x["code"]))
     )
     for i in db.scan()["Items"]:
         db.update_item(
